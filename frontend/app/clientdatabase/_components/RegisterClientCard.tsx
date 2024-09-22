d8c9904ac74e156a33f67ac5bed2dfbdf5511098
Wesley Kelvy <lilwkelvy@gmail.com>
2024-09-22 20:37:45 -0300

    feat: adding better mask for phone input and logic for add client to database form

--- frontend/app/clientdatabase/_components/RegisterClientCard.tsx
+++ frontend/app/clientdatabase/_components/RegisterClientCard.tsx
@@ -1,57 +1,46 @@
-import { Trash } from "@phosphor-icons/react";
 import { Button } from "../../../package/ui/src/button";
 import { Input } from "../../../package/ui/src/input";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
-
-interface cardClientProps {
-  name: string;
-  data: string;
-  email: string;
-  phone: string;
-  address: string;
-  scheduledVisits: string;
-  visitsMade: string;
-  isEditing?: boolean;
-}
+import { useHookFormMask } from "use-mask-input";
 
 const EditClientDataFormSchema = z.object({
   name: z.string().min(1, "Nome obrigatório"),
-  email: z.string().min(1, "Email obrigatório").email("Email Inválido"),
-  phone: z.string().min(1, "Telefone obrigatório"),
+  phone: z
+    .string({ required_error: "Telefone obrigatório" })
+    .min(11, "Telefone obrigatório"),
+  email: z.string().min(1, "Email obrigatório").email("E-mail inválido"),
   address: z.string().min(1, "Endereço obrigatória"),
 });
 
 type EditClientData = z.infer<typeof EditClientDataFormSchema>;
 
-export default function DetailedCardClient(props: cardClientProps) {
+export default function RegisterCardClient() {
   const {
     handleSubmit,
     register,
     formState: { errors },
-    reset,
   } = useForm<EditClientData>({
     resolver: zodResolver(EditClientDataFormSchema),
   });
-  const [isEditing, setIsEditing] = useState<boolean>(props.isEditing || false);
+  const registerWithMask = useHookFormMask(register);
 
-  useEffect(() => {
-    reset(props);
-  }, [props, reset]);
-
-  // Function to toggle editing mode
-  const toggleEditing = () => {
-    setIsEditing((prev) => !prev);
-    console.log(isEditing);
+  const submit = (data: any) => {
+    console.log("data:", data);
   };
 
   return (
     <section className="bg-cardClientBG flex flex-col space-y-4 w-full rounded-xl px-4 py-3 h-fit">
-      <form className="w-full h-fit justify-between">
-        {isEditing ? (
-          <div className="w-full h-fit flex flex-col space-y-4">
+      <form
+        className="w-full flex flex-col space-y-3 h-fit justify-between"
+        onSubmit={handleSubmit(submit)}
+      >
+        <h1>Registro de Cliente</h1>
+
+        <div className="w-full h-fit flex flex-col space-y-2">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-light">Nome</p>
             <Input
               id="name"
               placeholder={(errors.name && errors.name.message) || "Nome"}
@@ -61,7 +50,9 @@ export default function DetailedCardClient(props: cardClientProps) {
               }`}
               {...register("name")}
             />
-
+          </div>
+          <div className="flex flex-col space-y-2">
+            <p className="text-sm font-light">Email</p>
             <Input
               id="email"
               placeholder={(errors.email && errors.email.message) || "Email"}
@@ -71,7 +62,9 @@ export default function DetailedCardClient(props: cardClientProps) {
               }`}
               {...register("email")}
             />
-
+          </div>
+          <div className="flex flex-col space-y-2">
+            <p className="text-sm font-light">Telefone</p>
             <Input
               id="phone"
               placeholder={(errors.phone && errors.phone.message) || "Telefone"}
@@ -79,8 +72,12 @@ export default function DetailedCardClient(props: cardClientProps) {
               className={`pl-2 ${
                 errors.phone ? "placeholder:text-red-500" : "text-black"
               }`}
-              {...register("phone")}
+              {...registerWithMask("phone", ["(99) 9 9999-9999"])}
             />
+          </div>
+
+          <div className="flex flex-col space-y-2">
+            <h3 className="text-sm font-light">Enderenço</h3>
             <Input
               id="address"
               placeholder={
@@ -93,28 +90,9 @@ export default function DetailedCardClient(props: cardClientProps) {
               {...register("address")}
             />
           </div>
-        ) : (
-          <div className="w-full h-fit flex flex-col space-y-4">
-            <h1 className="text-2xl">Nome: {props.name}</h1>
-            <h2>Email: {props.email}</h2>
-            <h2>Telefone: {props.phone}</h2>
-            <h2>Endereço: {props.address}</h2>
-          </div>
-        )}
-      </form>
-      <div className="w-full h-full text-sm">
-        <h2>Visitas agendadas: {props.scheduledVisits}</h2>
-        <h2>Visitas feitas: {props.visitsMade}</h2>
-      </div>
-
-      {isEditing ? (
-        <div className="flex justify-center space-x-10 items-center">
-          <Button>Salvar</Button>
-          <Trash weight="fill" size={31} color="#F34213" />
         </div>
-      ) : (
-        <Button onClick={toggleEditing}>Editar</Button>
-      )}
+        <Button type="submit">Salvar</Button>
+      </form>
     </section>
   );
 }
--- frontend/app/clientdatabase/page.tsx
+++ frontend/app/clientdatabase/page.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 import arrowBack from "../../public/arrowBack.svg";
 import magnifier from "../../public/magnifier.svg";
 import CardClient from "./_components/CardClient";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavBar from "../components/navBar";
 import { PlusCircle } from "@phosphor-icons/react";
 import DetailedCardClient from "./_components/DetailedCardClient";
+import RegisterCardClient from "./_components/RegisterClientCard";
 
-interface client {
+interface Client {
   description: string;
   name: string;
   data: string;
@@ -17,14 +18,16 @@ interface client {
   phone: string;
   address: string;
   scheduledVisits: string;
-  VisitsMade: string;
+  visitsMade: string;
 }
 
 export default function PaymentsPage() {
   const [name, setName] = useState("");
   const [selectedClient, setSelectedClient] = useState<string | null>(null);
+  const [isRegisterCardOpen, setIsRegisterCardOpen] = useState(false); // Controle de visibilidade do card
+  const cardRef = useRef<HTMLDivElement | null>(null);
 
-  const clients = [
+  const clients: Client[] = [
     {
       description: "Manutenção de geladeira",
       name: "Wesley",
@@ -33,7 +36,7 @@ export default function PaymentsPage() {
       phone: "123456789",
       address: "Rua A, 123",
       scheduledVisits: "2",
-      VisitsMade: "1",
+      visitsMade: "1",
     },
     {
       description: "Manutenção de geladeira",
@@ -43,10 +46,29 @@ export default function PaymentsPage() {
       phone: "987654321",
       address: "Rua B, 456",
       scheduledVisits: "3",
-      VisitsMade: "3",
+      visitsMade: "3",
     },
     // more clients...
   ];
+
+  // Fecha o card se clicar fora dele
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+        setIsRegisterCardOpen(false);
+      }
+    }
+
+    if (isRegisterCardOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isRegisterCardOpen]);
 
   // Filtra a lista de clientes com base no nome digitado
   const filteredClients = clients.filter((client) =>
@@ -54,7 +76,16 @@ export default function PaymentsPage() {
   );
 
   return (
-    <main className="bg-main  h-screen w-full pt-16">
+    <main className="bg-main h-screen w-full pt-16 relative">
+      {/* RegisterCardClient que aparece ao clicar */}
+      {isRegisterCardOpen && (
+        <div className="absolute top-0 left-0 h-screen w-[100%] flex justify-center items-center bg-black bg-opacity-50 z-10">
+          <div className="w-[90%] sm:w-[60%] md:w-[40%]" ref={cardRef}>
+            <RegisterCardClient />
+          </div>
+        </div>
+      )}
+
       <article className="h-[92%] w-full space-y-5 px-6 flex flex-col ">
         <div className="flex justify-between w-full items-center ">
           <h1 className="text-xl">Base de Clientes</h1>
@@ -79,24 +110,13 @@ export default function PaymentsPage() {
               onChange={(e) => setName(e.target.value)}
             />
           </div>
-          <PlusCircle size={32} className="fill-whiteApp" />
+          {/* Botão que abre o card */}
+          <PlusCircle
+            size={32}
+            className="fill-whiteApp"
+            onClick={() => setIsRegisterCardOpen(true)}
+          />
         </section>
-
-        {/* Lista de clientes filtrados */}
-        {/* <section className="w-full h-fit rounded-xl space-y-3 overflow-scroll flex flex-col pb-6">
-          {filteredClients.length > 0 ? (
-            filteredClients.map((client, index) => (
-              <CardClient
-                key={index}
-                name={client.name}
-                description={client.description}
-              />
-            ))
-          ) : (
-            <p>Nenhum cliente encontrado.</p>
-          )}
-        </section> */}
-        {/* Conditionally render the detailed view or the client list */}
 
         <section className="w-full h-fit rounded-xl space-y-3 overflow-scroll flex flex-col pb-6">
           {filteredClients.length > 0 ? (
@@ -110,7 +130,7 @@ export default function PaymentsPage() {
                     phone={client.phone}
                     address={client.address}
                     scheduledVisits={client.scheduledVisits}
-                    VisitsMade={client.VisitsMade}
+                    visitsMade={client.visitsMade}
                     isEditing={false}
                   />
                 ) : (
@@ -129,4 +149,24 @@ export default function PaymentsPage() {
       <NavBar />
     </main>
   );
+}
+
+{
+  /* Lista de clientes filtrados */
+  /* <section className="w-full h-fit rounded-xl space-y-3 overflow-scroll flex flex-col pb-6">
+          {filteredClients.length > 0 ? (
+            filteredClients.map((client, index) => (
+              <CardClient
+                key={index}
+                name={client.name}
+                description={client.description}
+              />
+            ))
+          ) : (
+            <p>Nenhum cliente encontrado.</p>
+          )}
+        </section> */
+}
+{
+  /* Conditionally render the detailed view or the client list */
 }
