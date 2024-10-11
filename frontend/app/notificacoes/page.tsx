19c28de2b944d36f1e134af2ef7ecc8c79114c32
Wesley Kelvy <lilwkelvy@gmail.com>
2024-10-11 19:03:21 -0300

    feat: uploading last html changes

--- frontend/app/notificacoes/page.tsx
+++ frontend/app/notificacoes/page.tsx
@@ -5,12 +5,12 @@ import arrowBack from "../../public/arrowBack.svg";
 import magnifier from "../../public/magnifier.svg";
 import { useState } from "react";
 import NavBar from "../components/navBar";
-import CardClient from "./components/CardNotification";
+import CardNotification from "../pagamentos/components/CardNotification";
 
 export default function PaymentsPage() {
   const [name, setName] = useState("");
 
-  const clients = [
+  const notifications = [
     { data: "12/12/1221", name: "Wesley", isPayed: true },
     { data: "12/12/1221", name: "Maria", isPayed: false },
     { data: "12/12/1221", name: "João", isPayed: true },
@@ -26,15 +26,15 @@ export default function PaymentsPage() {
   ];
 
   // Filtra a lista de clientes com base no nome digitado
-  const filteredClients = clients.filter((client) =>
-    client.name.toLowerCase().includes(name.toLowerCase())
+  const filteredNotifications = notifications.filter((notifications) =>
+    notifications.name.toLowerCase().includes(name.toLowerCase())
   );
 
   return (
     <main className="bg-main h-screen w-full pt-16 relative text-whiteApp">
       <article className="h-[92%] w-full space-y-5 px-6 flex flex-col ">
         <div className="flex justify-between w-full items-center ">
-          <h1 className="text-xl">Registro de Pagamento</h1>
+          <h1 className="text-xl">Notificações</h1>
           <Image src={arrowBack} width={25} height={25} alt="arrowBackIcon" />
         </div>
 
@@ -60,13 +60,13 @@ export default function PaymentsPage() {
 
         {/* Lista de clientes filtrados */}
         <section className="w-full h-fit rounded-xl space-y-3 overflow-y-auto flex flex-col pb-6">
-          {filteredClients.length > 0 ? (
-            filteredClients.map((client, index) => (
-              <CardClient
+          {notifications.length > 0 ? (
+            notifications.map((notifications, index) => (
+              <CardNotification
                 key={index}
-                data={client.data}
-                name={client.name}
-                isPayed={client.isPayed}
+                data={notifications.data}
+                name={notifications.name}
+                isPayed={notifications.isPayed}
               />
             ))
           ) : (
