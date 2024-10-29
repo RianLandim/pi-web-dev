8a2894196ce0c8e8062f014000a2275847353074
Wesley Kelvy <lilwkelvy@gmail.com>
2024-10-29 16:14:10 -0300

    feat: added filter for names

--- frontend/app/pagamentos-gerais/page.tsx
+++ frontend/app/pagamentos-gerais/page.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import arrowBack from "@/public/arrowBack.svg";
 import NavBar from "../components/navBar";
 import GeneralPaymentCard from "./components/GeneralPaymentCard";
+import { PlusCircle } from "@phosphor-icons/react";
+import magnifier from "../../public/magnifier.svg";
 
 export default function ClientPaymentsPage({
   params,
@@ -65,11 +67,6 @@ export default function ClientPaymentsPage({
       value: "1000",
       paymentDeadline: "12/12/2024",
     },
-    // { data: "12/12/1221", name: "Wesley", isPayed: false },
-    // { data: "12/12/1221", name: "Wesley", isPayed: false },
-    // { data: "12/12/1221", name: "Wesley", isPayed: false },
-    // { data: "12/12/1221", name: "Wesley", isPayed: false },
-    // { data: "12/12/1221", name: "Wesley", isPayed: false },
   ];
 
   // Filtra a lista de clientes com base no nome digitado
@@ -82,9 +79,34 @@ export default function ClientPaymentsPage({
       <article className="h-[92%] w-full space-y-5 px-6 flex flex-col ">
         <div className="flex justify-between w-full items-center">
           {/*  I want to write the name of the cliente below */}
-          <h1 className="text-xl">Pagamento(s) do !client name! </h1>
+          <h1 className="text-xl">Pagamentos Gerais</h1>
           <Image src={arrowBack} width={25} height={25} alt="arrowBackIcon" />
         </div>
+        <section className="flex space-x-2 items-center h-fit">
+          <div className="flex w-full rounded-md bg-whiteApp">
+            <Image
+              className="bg-grayApp w-[15%] p-2 rounded-md"
+              src={magnifier}
+              width={25}
+              height={25}
+              alt="maginifierIcon"
+            />
+            <input
+              className="placeholder:pl-2 w-full text-black p-2 rounded-xl"
+              placeholder="Pesquise pelo nome..."
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          {/* Botão que abre o card de adicionar pagamento */}
+          {/* <PlusCircle
+            size={32}
+            className="fill-whiteApp"
+            onClick={() => setIsRegisterCardOpen(true)}
+          /> */}
+        </section>
 
         {/* Lista de clientes filtrados */}
         <section className="w-full h-fit rounded-xl space-y-3 overflow-y-auto flex flex-col pb-6">
