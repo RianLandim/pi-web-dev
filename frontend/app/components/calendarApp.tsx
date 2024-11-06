114333355ef39b4240292ba4ed1a929e05e31f1d
Wesley Kelvy <lilwkelvy@gmail.com>
2024-11-06 17:52:25 -0300

    feat: put the color and text structure for calendar events

--- frontend/app/components/calendarApp.tsx
+++ frontend/app/components/calendarApp.tsx
@@ -18,34 +18,136 @@ function CalendarApp({ onClickAgendaDate }: CalendarAppProps) {
 
   const events = [
     {
-      id: "1",
-      title: "Maquineta",
-      start: "2024-10-12",
-      end: "2024-10-12",
-      priority: "high",
+      id: 874574875,
+      start: "2024-09-09 07:45",
+      end: "2024-09-09 09:01",
+      _customContent: {
+        timeGrid: '<div class="custom-content">Custom Content</div>',
+        monthGrid: '<div class="custom-content">Custom Content</div>',
+      },
+      calendarId: 'work',
+
     },
     {
-      id: "2",
-      title: "Fusqueta",
-      start: "2024-10-12",
-      end: "2024-10-12",
-      priority: "low",
+      id: 874574875,
+      start: "2024-09-09",
+      end: "2024-09-09",
+      title: "All Day Event",
+      _customContent: {
+        dateGrid: '<div class="custom-content">Custom Content</div>',
+        monthAgenda: '<div class="custom-content">Custom Content</div>',
+      },
+      calendarId: 'work',
     },
     {
-      id: "3",
-      title: "Fusqueta",
-      start: "2024-10-12",
-      end: "2024-10-12",
+      id: 45678,
+      title: "Bi-Weekly Event Monday and Wednesday",
+      start: "2024-03-19 14:00",
+      end: "2024-03-19 15:00",
+      rrule: "FREQ=WEEKLY;INTERVAL=2;BYDAY=MO,WE;UNTIL=20240229T235959",
+      calendarId: 'work',
     },
     {
-      id: "4",
-      title: "Fusqueta",
-      start: "2024-10-12",
-      end: "2024-10-12",
+      id: 18547854,
+      title: "Bi-Weekly Event Monday and Wednesday",
+      start: "2024-02-05 14:00",
+      end: "2024-02-05 15:00",
+      rrule: "FREQ=WEEKLY;INTERVAL=2;BYDAY=MO,WE;UNTIL=20240229T235959",
+    },
+    {
+      id: 18547855,
+      title: "Weekly Event",
+      start: "2024-02-03",
+      end: "2024-02-03",
+      rrule: "FREQ=WEEKLY;COUNT=4",
+    },
+    {
+      id: 789,
+      title: "Daily event",
+      start: "2024-02-05 12:00",
+      end: "2024-02-05 13:55",
+      rrule: "FREQ=DAILY;COUNT=5",
+      calendarId: "personal",
+    },
+    {
+      id: 9834876578,
+      title: "Daily event 2",
+      start: "2024-02-05 12:00",
+      end: "2024-02-05 13:55",
+      rrule: "FREQ=DAILY;UNTIL=20240209T235900",
+      calendarId: "work",
+    },
+    {
+      id: 7845684678465874,
+      title: "Monthly event",
+      start: "2024-02-07 16:00",
+      end: "2024-02-07 17:55",
+      rrule: "FREQ=MONTHLY;COUNT=5",
+    },
+    {
+      rrule: "FREQ=YEARLY;COUNT=5",
+      title: "Yearly event",
+      start: "2024-02-08 16:00",
+      end: "2024-02-08 17:55",
+      id: 874367853,
     },
   ];
 
   const calendar = useNextCalendarApp({
+    calendars: {
+      personal: {
+        colorName: "personal",
+        lightColors: {
+          main: "#f9d71c",
+          container: "#fff5aa",
+          onContainer: "#594800",
+        },
+        darkColors: {
+          main: "#fff5c0",
+          onContainer: "#fff5de",
+          container: "#a29742",
+        },
+      },
+      work: {
+        colorName: "work",
+        lightColors: {
+          main: "#f91c45",
+          container: "#ffd2dc",
+          onContainer: "#59000d",
+        },
+        darkColors: {
+          main: "#ffc0cc",
+          onContainer: "#ffdee6",
+          container: "#a24258",
+        },
+      },
+      leisure: {
+        colorName: "leisure",
+        lightColors: {
+          main: "#1cf9b0",
+          container: "#dafff0",
+          onContainer: "#004d3d",
+        },
+        darkColors: {
+          main: "#c0fff5",
+          onContainer: "#e6fff5",
+          container: "#42a297",
+        },
+      },
+      school: {
+        colorName: "school",
+        lightColors: {
+          main: "#1c7df9",
+          container: "#d2e7ff",
+          onContainer: "#002859",
+        },
+        darkColors: {
+          main: "#c0dfff",
+          onContainer: "#dee6ff",
+          container: "#426aa2",
+        },
+      },
+    },
     callbacks: {
       onEventClick(calendarEvent) {
         console.log("onEventClick", calendarEvent);
